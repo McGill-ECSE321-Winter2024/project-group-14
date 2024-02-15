3b285b0091a3080d68a8291055e9556527e2d3c8
Loridy <loridyLuo@gmail.com>
2024-02-15 14:48:12 -0500

    Set up Spring Boot Project

---
+++ sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/SportCenterRegistrationApplication.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.sportCenterRegistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportCenterRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportCenterRegistrationApplication.class, args);
+	}
+
+}
---
+++ sportCenterRegistration/src/test/java/ca/mcgill/ecse321/sportCenterRegistration/SportCenterRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.mcgill.ecse321.sportCenterRegistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCenterRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
