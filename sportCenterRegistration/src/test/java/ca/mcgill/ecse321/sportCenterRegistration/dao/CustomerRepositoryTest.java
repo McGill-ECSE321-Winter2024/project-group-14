8fcdb5cd21c102ca7d3f1a1650b47cdcbac92244
Loridy <loridyLuo@gmail.com>
2024-02-17 01:10:19 -0500

    Update Tests for creating/reading Instructor, Owner, Customer by id

---
+++ sportCenterRegistration/src/test/java/ca/mcgill/ecse321/sportCenterRegistration/dao/CustomerRepositoryTest.java
@@ -0,0 +1,49 @@
+package ca.mcgill.ecse321.sportCenterRegistration.dao;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.sportCenterRegistration.model.Customer;
+
+@SpringBootTest
+public class CustomerRepositoryTest{
+    @Autowired
+    private CustomerRepository repo;
+
+    @BeforeEach
+    @AfterEach
+    public void clearDatabase() {
+        repo.deleteAll();
+    }
+
+    @Test
+    public void testCreateAndReadCustomer(){
+        // Create Customer
+        String username = "customer1";
+        String email = "customer1@gmail.com";
+        String password = "customer1";
+        Customer customer = new Customer(username, email, password);
+
+        // Save in the database
+        customer = repo.save(customer);
+        int customerId = customer.getId();
+
+        // Read from database
+        Customer result = repo.findCustomerById(customerId);
+
+        assertNotNull(result);
+        assertEquals(customerId, result.getId());
+        assertEquals(username, result.getUsername());
+        assertEquals(email, result.getEmail());
+        assertEquals(password, result.getPassword());
+
+    }
+}
---
+++ sportCenterRegistration/src/test/java/ca/mcgill/ecse321/sportCenterRegistration/dao/InstructorRepositoryTest.java
@@ -0,0 +1,49 @@
+package ca.mcgill.ecse321.sportCenterRegistration.dao;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import ca.mcgill.ecse321.sportCenterRegistration.model.Instructor;
+
+@SpringBootTest
+public class InstructorRepositoryTest{
+    @Autowired
+    private InstructorRepository repo;
+
+    @BeforeEach
+    @AfterEach
+    public void clearDatabase() {
+        repo.deleteAll();
+    }
+
+    @Test
+    public void testCreateAndReadInstructor(){
+        // Create Instructor
+        String username = "instructor1";
+        String email = "instructor1@gmail.com";
+        String password = "instructor1";
+        Instructor instructor = new Instructor(username, email, password);
+
+        // Save in the database
+        instructor = repo.save(instructor);
+        int instructorId = instructor.getId();
+
+        // Read from database
+        Instructor result = repo.findInstructorById(instructorId);
+
+        assertNotNull(result);
+        assertEquals(instructorId, result.getId());
+        assertEquals(username, result.getUsername());
+        assertEquals(email, result.getEmail());
+        assertEquals(password, result.getPassword());
+
+    }
+}
