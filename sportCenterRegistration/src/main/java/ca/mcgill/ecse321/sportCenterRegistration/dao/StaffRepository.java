011ba22c6c10835a94f3ef9a4c8dc6b4869a8b51
Loridy <loridyLuo@gmail.com>
2024-02-18 04:45:35 -0500

    fix error on staff test

--- sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/dao/StaffRepository.java
+++ sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/dao/StaffRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.sportCenterRegistration.model.Staff;
 
 public interface StaffRepository extends CrudRepository<Staff, Integer>{
-//    public Staff findStaffById(int id);
+    public Staff findStaffById(int id);
 
 }
