3817b2dcd797581658bc7b691f724a5d71d5997c
hammad0860 <muhammad.h0860@gmail.com>
2024-03-14 03:11:32 -0400

    Update SportCenterRegistrationRestController.java

--- sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/controller/SportCenterRegistrationRestController.java
+++ sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/controller/SportCenterRegistrationRestController.java
@@ -2,6 +2,8 @@ package ca.mcgill.ecse321.sportCenterRegistration.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.mcgill.ecse321.sportCenterRegistration.service.SportCenterRegistrationService;
@@ -12,4 +14,29 @@ public class SportCenterRegistrationRestController {
 
 	@Autowired
 	private SportCenterRegistrationService service;
+
+
+	@DeleteMapping(value= {"/customer/{username}", "/customer/{username}/"})
+	public void deleteCustomer(@PathVariable("username") String username) throws 
+	IllegalArgumentException {
+		service.deleteCustomer(username);
+	}
+
+
+	@DeleteMapping(value= {"/instructor/{username}", "/instructor/{username}/"})
+	public void deleteInstructor(@PathVariable("username") String username) throws 
+	IllegalArgumentException {
+		service.deleteInstructor(username);
+	}
+
+
+
+	
+	
+	
+
+	
+
+	
+
 }
