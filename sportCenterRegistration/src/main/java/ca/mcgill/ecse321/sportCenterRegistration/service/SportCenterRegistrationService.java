a5de36bad6a54a525e1e50e30b936febffbbd20c
stephh12 <wensheng.huang2@mail.mcgill.ca>
2024-03-15 19:45:20 -0400

    Update SportCenterRegistrationService.java

--- sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/service/SportCenterRegistrationService.java
+++ sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/service/SportCenterRegistrationService.java
@@ -21,7 +21,6 @@ import ca.mcgill.ecse321.sportCenterRegistration.dao.ShiftRepository;
 import ca.mcgill.ecse321.sportCenterRegistration.dao.SportClassRepository;
 import ca.mcgill.ecse321.sportCenterRegistration.dao.StaffRepository;
 
-
 import ca.mcgill.ecse321.sportCenterRegistration.model.Account;
 import ca.mcgill.ecse321.sportCenterRegistration.model.Customer;
 import ca.mcgill.ecse321.sportCenterRegistration.model.Instructor;
@@ -41,187 +40,234 @@ public class SportCenterRegistrationService {
 	CustomerRepository CustomerRepository;
 	@Autowired
 	InstructorRepository InstructorRepository;
-    @Autowired
+	@Autowired
 	OwnerRepository OwnerRepository;
 	@Autowired
 	RegistrationRepository RegistrationRepository;
 	@Autowired
 	SessionRepository SessionRepository;
-    @Autowired
+	@Autowired
 	ShiftRepository ShiftRepository;
 	@Autowired
 	SportClassRepository SportClassRepository;
 	@Autowired
 	StaffRepository StaffRepository;
 
+	/*
+	 * note: all the below are going to be our wrapper classes annotated with
+	 * transactional. If you use
+	 * something that should be wrapped just add it in here
+	 */
 
+	/**
+	 * Section: Session servive
+	 * Author: Stephen Huang
+	 * This method creates a session schedual
+	 */
 
-    /*note: all the below are going to be our wrapper classes annotated with transactional. If you use 
-    something that should be wrapped just add it in here */
-
-
-
-    // Author: Stephen Huang (use case: change schedule)
 	@Transactional
 	public Session createSession(Time startTime, Time endTime, String location, Date date) {
 
+		// checking all inputs are valid
 		String error = "";
- 		if (location == null) {
-			error = error + "Session location cannot be empty! ";
+		if (location == null) {
+			throw new IllegalArgumentException("Session location cannot be empty!");
 		}
 		if (date == null) {
-			error = error + "Session date cannot be empty! ";
+			throw new IllegalArgumentException("Session date cannot be empty!");
 		}
 		if (startTime == null) {
-			error = error + "Session start time cannot be empty! ";
+			throw new IllegalArgumentException("Session start time cannot be empty!");
 		}
 		if (endTime == null) {
-			error = error + "Session end time cannot be empty! ";
+			throw new IllegalArgumentException("Session end time cannot be empty!");
 		}
 		if (endTime != null && startTime != null && endTime.before(startTime)) {
-			error = error + "Session end time cannot be before Session start time! ";
+			throw new IllegalArgumentException("Session end time cannot be before Session start time! ");
 		}
 
+		// create a session
 		Session session = new Session();
-
 		session.setDate(date);
 		session.setStartTime(startTime);
 		session.setEndTime(endTime);
 		session.setLocation(location);
+		Session newSession = SessionRepository.save(session);
+		return newSession;
+	}
 
-	    SessionRepository.save(session);
+	/**
+	 * Section: Session servive
+	 * Author: Stephen Huang
+	 * This method updates a session schedual
+	 */
 
-		return session;
-}
+	public Session updateSession(Long id, Time startTime, Time endTime, String location, Date date) {
 
-	@Transactional
-	public List<Session> getAllSessions() {
-		return toList(SessionRepository.findAll());
-}
+		// check if the session exists
+		Session session = SessionRepository.findSessionById(id);
+		if (session == null) {
+			throw new IllegalArgumentException("No Session found");
+		}
 
-	@Transactional
-	public Session getSessionByDate(Date date) {
+		// check if all the inputs are vaild
+		String error = "";
+		if (location == null) {
+			throw new IllegalArgumentException("Session location cannot be empty!");
+		}
 		if (date == null) {
-			throw new IllegalArgumentException("Date cannot be empty!");
+			throw new IllegalArgumentException("Session date cannot be empty!");
 		}
-		for (Session sn : SessionRepository.findAll()) {
-			if (sn.getDate().equals(date)) {
-				return sn;
-			}
+		if (startTime == null) {
+			throw new IllegalArgumentException("Session start time cannot be empty!");
 		}
-		return null;
-	}
-		
-	@Transactional
-	public Session getSessionByStaffAndDate(String username, Date date) {
-
-		Account account = getAccount(username);
-		if (account != null && account.getStaff() ) {
-			Shift shift = getShiftByOwner(username);
-			if ( shift!= null) {
-				for (Session sn : shift.getSession()) {
-					if (sn.getDate().equals(date)) {
-						return sn;
-					}
-				}
-			} else {
-				throw new IllegalArgumentException("Owner is not assigned a schedule.");
-			}
-		} else {
-			throw new IllegalArgumentException("The username does not belong to any Owner.");
+		if (endTime == null) {
+			throw new IllegalArgumentException("Session end time cannot be empty!");
 		}
-		return null;
-	}
-
-	@Transactional
-	public Session updateSessionByStaffAndDate(String username, DayOfWeek dayOfWeek, Time startTime,
-			Time endTime) {
-
-		Session sn = getSessionByStaffAndDate(username, dayOfWeek);
-
-		sn.setStartTime(startTime);
-		sn.setEndTime(endTime);
-		SessionRepository.save(sn);
-
-		return sn;
-	}
-	// finish (use case: change schedule)
-    
-
-
-
-	
-    /*
-	@Transactional
-	public Person createPerson(String name) {
-		Person person = new Person();
-		person.setName(name);
-		personRepository.save(person);
-		return person;
-	}
-
-	@Transactional
-	public Person getPerson(String name) {
-		Person person = personRepository.findPersonByName(name);
-		return person;
-	}
-
-	@Transactional
-	public List<Person> getAllPersons() {
-		return toList(personRepository.findAll());
-	}
-
-	@Transactional
-	public Event createEvent(String name, Date date, Time startTime, Time endTime) {
-		Event event = new Event();
-		event.setName(name);
-		event.setDate(date);
-		event.setStartTime(startTime);
-		event.setEndTime(endTime);
-		eventRepository.save(event);
-		return event;
-	}
-
-	@Transactional
-	public Event getEvent(String name) {
-		Event event = eventRepository.findEventByName(name);
-		return event;
-	}
-
-	@Transactional
-	public List<Event> getAllEvents() {
-		return toList(eventRepository.findAll());
-	}
-
-	@Transactional
-	public Registration register(Person person, Event event) {
-		Registration registration = new Registration();
-		registration.setId(person.getName().hashCode() * event.getName().hashCode());
-		registration.setPerson(person);
-		registration.setEvent(event);
-
-		registrationRepository.save(registration);
-
-		return registration;
-	}
-
-	@Transactional
-	public List<Registration> getAllRegistrations(){
-		return toList(registrationRepository.findAll());
-	}
-
-	@Transactional
-	public List<Event> getEventsAttendedByPerson(Person person) {
-		List<Event> eventsAttendedByPerson = new ArrayList<>();
-		for (Registration r : registrationRepository.findByPerson(person)) {
-			eventsAttendedByPerson.add(r.getEvent());
+		if (endTime != null && startTime != null && endTime.before(startTime)) {
+			throw new IllegalArgumentException("Session end time cannot be before Session start time! ");
 		}
-		return eventsAttendedByPerson;
+
+		// update session
+		session.setDate(date);
+		session.setStartTime(startTime);
+		session.setEndTime(endTime);
+		session.setLocation(location);
+		SessionRepository.save(session);
+		return session;
 	}
 
-*/
+	/**
+	 * Section: Session servive
+	 * Author: Stephen Huang
+	 * This method gets a specific session schedule given the corresponding id
+	 */
+	@Transactional
+	public Session getSession(int id) {
 
-	private <T> List<T> toList(Iterable<T> iterable){
+		// return dailyschedule with the inputed id
+		Session session = SessionRepository.findSessionById(id);
+		return session;
+	}
+
+	/**
+	 * Section: Session servive
+	 * Author: Stephen Huang
+	 * This method deletes a session schedual
+	 */
+	@Transactional
+	public boolean deleteSession(int id) {
+
+		// check null id
+		if (id = null) { // help me here !!!!!!!!!! LMAO
+			throw new IllegalArgumentException("Id cannot be empty");
+		}
+
+		// check if the daily schedule exists
+		Session session = SessionRepository.findSessionById(id);
+
+		if (session == null)
+			throw new IllegalArgumentException("Session not found");
+
+		// delete dailyschedule
+		SessionRepository.delete(session);
+		session.delete();
+		return true;
+	}
+
+	/**
+	 * Section: Session servive
+	 * Author: Stephen Huang
+	 * This method gets all session
+	 */
+	@Transactional
+	public List<Session> getAllSession() {
+
+		// return all session in the system
+		return toList(SessionRepository.findAll());
+	}
+
+	private <T> List<T> toList(Iterable<T> iterable) {
+		List<T> resultList = new ArrayList<T>();
+		for (T t : iterable) {
+			resultList.add(t);
+		}
+		return resultList;
+
+	}
+
+	/*
+	 * @Transactional
+	 * public Person createPerson(String name) {
+	 * Person person = new Person();
+	 * person.setName(name);
+	 * personRepository.save(person);
+	 * return person;
+	 * }
+	 * 
+	 * @Transactional
+	 * public Person getPerson(String name) {
+	 * Person person = personRepository.findPersonByName(name);
+	 * return person;
+	 * }
+	 * 
+	 * @Transactional
+	 * public List<Person> getAllPersons() {
+	 * return toList(personRepository.findAll());
+	 * }
+	 * 
+	 * @Transactional
+	 * public Event createEvent(String name, Date date, Time startTime, Time
+	 * endTime) {
+	 * Event event = new Event();
+	 * event.setName(name);
+	 * event.setDate(date);
+	 * event.setStartTime(startTime);
+	 * event.setEndTime(endTime);
+	 * eventRepository.save(event);
+	 * return event;
+	 * }
+	 * 
+	 * @Transactional
+	 * public Event getEvent(String name) {
+	 * Event event = eventRepository.findEventByName(name);
+	 * return event;
+	 * }
+	 * 
+	 * @Transactional
+	 * public List<Event> getAllEvents() {
+	 * return toList(eventRepository.findAll());
+	 * }
+	 * 
+	 * @Transactional
+	 * public Registration register(Person person, Event event) {
+	 * Registration registration = new Registration();
+	 * registration.setId(person.getName().hashCode() * event.getName().hashCode());
+	 * registration.setPerson(person);
+	 * registration.setEvent(event);
+	 * 
+	 * registrationRepository.save(registration);
+	 * 
+	 * return registration;
+	 * }
+	 * 
+	 * @Transactional
+	 * public List<Registration> getAllRegistrations(){
+	 * return toList(registrationRepository.findAll());
+	 * }
+	 * 
+	 * @Transactional
+	 * public List<Event> getEventsAttendedByPerson(Person person) {
+	 * List<Event> eventsAttendedByPerson = new ArrayList<>();
+	 * for (Registration r : registrationRepository.findByPerson(person)) {
+	 * eventsAttendedByPerson.add(r.getEvent());
+	 * }
+	 * return eventsAttendedByPerson;
+	 * }
+	 * 
+	 */
+
+	private <T> List<T> toList(Iterable<T> iterable) {
 		List<T> resultList = new ArrayList<T>();
 		for (T t : iterable) {
 			resultList.add(t);
