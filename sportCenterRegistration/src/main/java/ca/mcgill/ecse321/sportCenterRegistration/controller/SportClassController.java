97de05ae684aa68d6a79a2bf94d1b45cbfb0f429
Loridy <loridyLuo@gmail.com>
2024-03-16 22:26:00 -0400

    Create SportClassController.java

---
+++ sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/controller/SportClassController.java
@@ -0,0 +1,52 @@
+package ca.mcgill.ecse321.sportCenterRegistration.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.sportCenterRegistration.dto.SportClassDto;
+import ca.mcgill.ecse321.sportCenterRegistration.model.SportClass;
+import ca.mcgill.ecse321.sportCenterRegistration.service.SportClassService;
+import ca.mcgill.ecse321.sportCenterRegistration.service.InstructorService;
+import ca.mcgill.ecse321.sportCenterRegistration.service.OwnerService;
+@RestController
+public class SportClassController{
+    @Autowired
+    private SportClassService sportClassService;
+    @Autowired
+    private InstructorService instructorService;
+    @Autowired
+    private OwnerService ownerService;
+
+    @GetMapping(value="/sport-class/{name}", "sport-class/{name}/")
+    public SportClassDto getSportClass(@PathVariable("name") String name) throws IllegalArgumentException{
+        SportClass sportClass = sportClassService.getSportClass(name);
+        return convertToDto(sportClass);
+    }
+
+    @PostMapping(value="/sport-class/{name}", "sport-class/{name}/")
+    public SportClassDto createSportClass(@PathVariable("name") String name) throws IllegalArgumentException{
+        SportClass sportClass = instructorService.createSportClass(name);
+        return convertToDto(sportClass);
+    }
+
+    @PutMapping(value="/sport-class/approve/{name}", "sport-class/approve/{name}/")
+    public SportClassDto approveSportClass(@PathVariable("name") String name) throws IllegalArgumentException{
+        SportClass sportClass = ownerService.approveSportClass(name);
+        return convertToDto(sportClass);
+    }
+
+    private SportClassDto convertToDto(SportClass s) throws IllegalArgumentExeception{
+        return new SportClassDto(s.getName(), s.getApproved());
+    }
+}
