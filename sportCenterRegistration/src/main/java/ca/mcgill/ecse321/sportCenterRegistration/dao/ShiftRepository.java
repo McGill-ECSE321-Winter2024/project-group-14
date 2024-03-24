2011fe68af9831c4752f68bf253df67ce840a447
ming-x-yue <114600503+ming-x-yue@users.noreply.github.com>
2024-03-24 13:10:12 -0400

    Update ShiftRepository.java

--- sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/dao/ShiftRepository.java
+++ sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/dao/ShiftRepository.java
@@ -8,8 +8,10 @@ import org.springframework.data.repository.CrudRepository;
 import ca.mcgill.ecse321.sportCenterRegistration.model.Shift;
 import ca.mcgill.ecse321.sportCenterRegistration.model.Staff;
 
+
 public interface ShiftRepository extends CrudRepository<Shift, Integer>{
     public Shift findShiftById(int id);
     public List<Shift> findShiftByStaffAndDate(Staff staff, Date date);
-
+    public List<Shift> findShiftByDate(Date date);
+    public List<Shift> findShiftByStaff(Staff staff);
 }
