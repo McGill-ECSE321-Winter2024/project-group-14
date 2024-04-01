7904feb23eab05aa0b88482f1fc3e471a9ce8ab2
Loridy <loridyLuo@gmail.com>
2024-04-01 02:48:27 -0400

    Update Login Controller

--- sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/controller/LoginController.java
+++ sportCenterRegistration/src/main/java/ca/mcgill/ecse321/sportCenterRegistration/controller/LoginController.java
@@ -3,7 +3,7 @@ package ca.mcgill.ecse321.sportCenterRegistration.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,11 +22,11 @@ public class LoginController {
 	@Autowired
 	private LoginService loginService;
 
-	@PostMapping(value = { "/login", "/login/" })
-	public ResponseEntity<?> login(@RequestParam String email, @RequestParam String password) {
+	@GetMapping(value = { "/login/email", "/login/email/" })
+	public ResponseEntity<?> loginByEmail(@RequestParam String email, @RequestParam String password) {
 		Account user = null;
 		try {
-			user = loginService.login(email, password);
+			user = loginService.loginByEmail(email, password);
 		} catch (IllegalArgumentException e) {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
@@ -46,6 +46,32 @@ public class LoginController {
 		}
 		return null;
 	}
+
+	@GetMapping(value = { "/login/username", "/login/username/" })
+	public ResponseEntity<?> loginByUsername(@RequestParam String username, @RequestParam String password) {
+		Account user = null;
+		try {
+			user = loginService.loginByUsername(username, password);
+		} catch (IllegalArgumentException e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		// Determine what type of user is login in in the system.
+		if (user instanceof Customer) {
+
+			return new ResponseEntity<>(convertToCustomerDto((Customer) user), HttpStatus.OK);
+		}
+
+		if (user instanceof Instructor) {
+			return new ResponseEntity<>(convertToInstructorDto((Instructor) user), HttpStatus.OK);
+		}
+
+		if (user instanceof Owner) {
+			return new ResponseEntity<>(convertToOwnerDTO((Owner) user), HttpStatus.OK);
+		}
+		return null;
+	}
+
 
 	public static CustomerDTO convertToCustomerDto(Customer customer) {
 		if (customer == null) {
