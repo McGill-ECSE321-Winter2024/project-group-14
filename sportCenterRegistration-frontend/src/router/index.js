95a3624a25f161f229f9fe0a1708dc66c74e3080
Loridy <loridyLuo@gmail.com>
2024-04-12 00:56:37 -0400

    update path file

--- sportCenterRegistration-frontend/src/router/index.js
+++ sportCenterRegistration-frontend/src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
-import ManageInstructor from '@/components/ManageInstructor'
-import ManageCustomer from '@/components/ManageCustomer'
+import ManageInstructor from '@/components/Owner/ManageInstructor'
+import ManageCustomer from '@/components/Owner/ManageCustomer'
 import OwnerApp from '@/components/OwnerApp'
+import CustomerApp from '@/components/CustomerApp'
+import AddClasses from '@/components/Customer/AddClasses'
+import ReviewClasses from '@/components/Customer/ReviewClasses'
 import LoginPage from '@/components/LoginPage'
 import SignupPage from '@/components/SignupPage'
 
@@ -19,6 +22,16 @@ export default new Router({
       children: [
         { path: 'manageInstructor', name: 'ManageInstructor', component: ManageInstructor },
         { path: 'manageCustomer', name: 'ManageCustomer', component: ManageCustomer },
+      ]
+    },
+    {
+      path: '/customerApp/',
+      name: 'CustomerApp',
+      component: CustomerApp,
+      // redirect: '/home',
+      children: [
+        { path: 'addClasses', name: 'AddClasses', component: AddClasses },
+        { path: 'reviewClasses', name: 'ReviewClasses', component: ReviewClasses },
       ]
     },
     {
