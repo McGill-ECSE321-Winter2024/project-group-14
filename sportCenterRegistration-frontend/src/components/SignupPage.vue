43a93720831bde637ecc25fb87e05e21dba26f60
hammad0860 <muhammad.h0860@gmail.com>
2024-04-12 15:01:16 -0400

    changed sign up colors

--- sportCenterRegistration-frontend/src/components/SignupPage.vue
+++ sportCenterRegistration-frontend/src/components/SignupPage.vue
@@ -120,11 +120,7 @@ h5,
   box-sizing: border-box;
 }
 .sign-up-page {
-  background: linear-gradient(
-    180deg,
-    rgba(97, 237, 237, 1) 0%,
-    rgba(153, 153, 153, 1) 100%
-  );
+  background: linear-gradient(to right, #cccccc, #003366);
   height: 1024px;
   position: relative;
   overflow: hidden;
@@ -319,11 +315,7 @@ h5,
   position: relative;
 }
 .welcome {
-  background: linear-gradient(
-    108.92deg,
-    rgba(223, 112, 132, 1) 10.000000149011612%,
-    rgba(255, 0, 153, 1) 100%
-  );
+  background: linear-gradient(to bottom, #ffffff, #f0f0f0);
   background-clip: text;
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
